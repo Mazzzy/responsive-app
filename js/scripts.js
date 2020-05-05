c6f6f7cc9bbe9d33482615910c7e46ad95ad4e1f
Mazahar Shaikh <shaikhmazahar@gmail.com>
2020-05-05 17:02:14 +0530

    Added html css files

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+    (function ($) {
+    "use strict"; // Start of use strict
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+})(jQuery); // End of use strict
